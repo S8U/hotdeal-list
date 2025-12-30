526f8a1e51cdb9fc8034b5982c1ece5ecebb864b
S8U <0uh1004@gmail.com>
2025-12-31 06:55:14 +0900

    Initial commit

---
+++ backend/src/main/kotlin/com/github/s8u/hotdeallist/HotdealListApplication.kt
@@ -0,0 +1,11 @@
+package com.github.s8u.hotdeallist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotdealListApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotdealListApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/github/s8u/hotdeallist/HotdealListApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.s8u.hotdeallist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotdealListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
