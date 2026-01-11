23d72f1e499484a6896f3833f3b274d52af2c979
S8U <0uh1004@gmail.com>
2026-01-11 16:58:58 +0900

    fix: 핫딜이 없을 때 업데이트 시도하는 오류 수정

--- backend/src/main/kotlin/com/github/s8u/hotdeallist/service/HotdealServiceFacade.kt
+++ backend/src/main/kotlin/com/github/s8u/hotdeallist/service/HotdealServiceFacade.kt
@@ -1,11 +1,15 @@
 package com.github.s8u.hotdeallist.service
 
 import com.github.s8u.hotdeallist.enums.PlatformType
+import com.github.s8u.hotdeallist.repository.HotdealProcessRepository
+import com.github.s8u.hotdeallist.repository.HotdealRepository
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class HotdealServiceFacade(
+    private val hotdealRepository: HotdealRepository,
+    private val hotdealProcessRepository: HotdealProcessRepository,
     private val hotdealRawService: HotdealRawService,
     private val hotdealService: HotdealService,
     private val hotdealProcessService: HotdealProcessService
@@ -26,22 +30,25 @@ class HotdealServiceFacade(
     fun createHotdeal(platformType: PlatformType, page: Int, delay: Long = 1000L) {
         logger.info("Creating hotdeal platformType={}, page={}", platformType, page)
 
-        // 크롤링
+        // 핫딜 크롤링
         val rawIds = hotdealRawService.crawlHotdealRaw(platformType, page, delay)
 
-        // 생성
-        rawIds.forEach { rawId, isCreate ->
-            // 생성
-            if (isCreate) {
-                // 핫딜 데이터 가공
-                hotdealProcessService.processHotdealFromRaw(rawId)
+        // 핫딜 저장
+        rawIds.forEach { rawId, _ ->
+            val hotdealProcess = hotdealProcessRepository.findFirstByHotdealRawIdOrderByIdDesc(rawId)
+            val hotdeal = hotdealRepository.findByHotdealRawId(rawId)
 
-                // 핫딜 생성
+            // 핫딜 데이터 가공
+            if (hotdealProcess == null) {
+                hotdealProcessService.processHotdealFromRaw(rawId)
+            }
+
+            // 핫딜 생성
+            if (hotdeal == null) {
                 hotdealService.createHotdealFromRawAndProcess(rawId)
             }
-            // 업데이트
+            // 핫딜 업데이트
             else {
-                // 핫딜 데이터 업데이트
                 hotdealService.updateHotdealFromRaw(rawId)
             }
         }
